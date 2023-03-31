dc36a8b9db76e76b64f97cce8ca705afa2786f83
Jonathan Amiez <jonathan.amiez@gmail.com>
2023-03-31 19:25:46 +0200

    Add init helper

--- flake.nix
+++ flake.nix
@@ -7,7 +7,14 @@
   let
     system = "x86_64-linux";
     pkgs = import inputs.nixpkgs { inherit system; };
+
+    k8s-tanka-init = pkgs.writeShellScriptBin "k8s-tanka-init" ''
+      ${pkgs.tanka}/bin/tk tool charts vendor
+      ${pkgs.kind}/bin/kind create cluster --config cluster.yaml --kubeconfig .kubecfg
+    '';
+
     dependencies = with pkgs; [
+      k8s-tanka-init
       jsonnet-bundler
       kind
       kubectl
