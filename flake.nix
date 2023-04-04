f2b3386639d1807a93fa227dd99c06a652f7120a
Jonathan Amiez <jonathan.amiez@gmail.com>
2023-04-04 13:54:38 +0200

    Improve init script

--- flake.nix
+++ flake.nix
@@ -7,14 +7,21 @@
   let
     system = "x86_64-linux";
     pkgs = import inputs.nixpkgs { inherit system; };
+    cluster-name = "k8s-tanka-starter";
 
     k8s-tanka-init = pkgs.writeShellScriptBin "k8s-tanka-init" ''
       ${pkgs.tanka}/bin/tk tool charts vendor
-      ${pkgs.kind}/bin/kind create cluster --config cluster.yaml --kubeconfig .kubecfg
+      ${pkgs.kind}/bin/kind create cluster --name ${cluster-name} --config cluster.yaml --kubeconfig .kubecfg
+      ${pkgs.tanka}/bin/tk apply environments/kind --auto-approve always
+    '';
+
+    k8s-tanka-cleanup = pkgs.writeShellScriptBin "k8s-tanka-cleanup" ''
+      ${pkgs.kind}/bin/kind delete cluster --name ${cluster-name}
     '';
 
     dependencies = with pkgs; [
       k8s-tanka-init
+      k8s-tanka-cleanup
       jsonnet-bundler
       kind
       kubectl
